47a3375aea4b5cb311932ff16f829f0de2faddb2
gattaka <gattakahynca@seznam.cz>
2018-02-24 13:33:00 +0100

    Multiupload poll interval improved

--- src/main/java/cz/gattserver/web/common/ui/MultiUpload.java
+++ src/main/java/cz/gattserver/web/common/ui/MultiUpload.java
@@ -3,6 +3,7 @@ package cz.gattserver.web.common.ui;
 import java.io.InputStream;
 
 import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.UI;
 import com.wcs.wcslib.vaadin.widget.multifileupload.ui.MultiFileUpload;
 import com.wcs.wcslib.vaadin.widget.multifileupload.ui.UploadStateWindow;
 
@@ -13,12 +14,19 @@ public abstract class MultiUpload extends CssLayout {
 	private static final String DEFAULT_SINGLE_BUTTON_CAPTION = "Vybrat soubor";
 	private static final String DEFAULT_MULTI_BUTTON_CAPTION = "Vybrat soubory";
 
+	private UI ui;
 	protected MultiFileUpload multiFileUpload;
 	protected UploadStateWindow stateWindow;
 
-	protected abstract void handleFile(InputStream in, String fileName, String mimeType, long length);
+	protected abstract void handleFile(InputStream in, String fileName, String mimeType, long length,
+			int filesLeftInQueue);
 
 	protected void onStart() {
+		ui.setPollInterval(200);
+	}
+
+	protected void onFinish() {
+		ui.setPollInterval(-1);
 	}
 
 	@Override
@@ -35,11 +43,9 @@ public abstract class MultiUpload extends CssLayout {
 	}
 
 	public MultiUpload(String caption, boolean multiple) {
+		ui = UI.getCurrent();
 		stateWindow = new UploadStateWindow();
-		multiFileUpload = new MultiFileUpload(this::onStart,
-				(InputStream stream, String fileName, String mimeType, long length,
-						int filesLeftInQueue) -> MultiUpload.this.handleFile(stream, fileName, mimeType, length),
-				stateWindow, multiple);
+		multiFileUpload = new MultiFileUpload(this::onStart, this::handleFile, stateWindow, multiple);
 		multiFileUpload.setWidth(null);
 		multiFileUpload.setUploadButtonIcon(ImageIcon.UP_16_ICON.createResource());
 		multiFileUpload.setUploadButtonCaptions(caption == null ? DEFAULT_SINGLE_BUTTON_CAPTION : caption,
