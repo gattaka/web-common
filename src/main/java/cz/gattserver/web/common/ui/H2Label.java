e44bede9d13b8dda3b2356a9fba1dcccd7b2beae
gattaka <gattakahynca@seznam.cz>
2018-03-04 12:29:21 +0100

    H2 label fixed

--- src/main/java/cz/gattserver/web/common/ui/H2Label.java
+++ src/main/java/cz/gattserver/web/common/ui/H2Label.java
@@ -1,5 +1,7 @@
 package cz.gattserver.web.common.ui;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.Label;
 
@@ -8,18 +10,21 @@ public class H2Label extends Label {
 	private static final long serialVersionUID = 7366248946391250891L;
 
 	public H2Label() {
+		this(null);
 	}
 
 	public H2Label(String text) {
 		setWidth("100%");
+		setValue(text);
 		setContentMode(ContentMode.HTML);
-		if (text != null)
-			setValue(text);
 	}
 
 	@Override
 	public void setValue(String text) {
-		super.setValue("<h2>" + text + "</h2>");
+		if (StringUtils.isNotBlank(text))
+			super.setValue("<h2>" + text + "</h2>");
+		else
+			super.setValue(null);
 	}
 
 }
--- src/main/java/cz/gattserver/web/common/ui/MultiUpload.java
+++ src/main/java/cz/gattserver/web/common/ui/MultiUpload.java
@@ -18,15 +18,59 @@ public abstract class MultiUpload extends CssLayout {
 	protected MultiFileUpload multiFileUpload;
 	protected UploadStateWindow stateWindow;
 
-	protected abstract void handleFile(InputStream in, String fileName, String mimeType, long length,
-			int filesLeftInQueue);
+	private boolean queueStarted = false;
 
-	protected void onStart() {
-		ui.setPollInterval(200);
+	/**
+	 * Dokončení nahrávání souboru z fronty
+	 * 
+	 * @param in
+	 *            datový tok souboru
+	 * @param fileName
+	 *            název souboru
+	 * @param mimeType
+	 *            mime typ souboru
+	 * @param length
+	 *            délka souboru
+	 * @param filesLeftInQueue
+	 *            počet zbývajících souborů ve frontě
+	 */
+	protected void fileUploadFinished(InputStream in, String fileName, String mimeType, long length,
+			int filesLeftInQueue) {
+		if (filesLeftInQueue == 0)
+			queueFinished();
 	}
 
-	protected void onFinish() {
+	protected void queueFinished() {
+		// ui.setPollInterval(-1);
+		queueStarted = false;
+	}
+
+	/**
+	 * Zahájení nahrávání souboru z fronty
+	 */
+	protected void fileUploadStarted() {
+		if (!queueStarted)
+			queueStarted();
+	}
+
+	/**
+	 * Zahájení nahrávání fronty
+	 */
+	protected void queueStarted() {
+		// ui.setPollInterval(200);
+		queueStarted = true;
+	}
+
+	@Override
+	public void attach() {
+		ui.setPollInterval(200);
+		super.attach();
+	}
+
+	@Override
+	public void detach() {
 		ui.setPollInterval(-1);
+		super.detach();
 	}
 
 	@Override
@@ -45,7 +89,7 @@ public abstract class MultiUpload extends CssLayout {
 	public MultiUpload(String caption, boolean multiple) {
 		ui = UI.getCurrent();
 		stateWindow = new UploadStateWindow();
-		multiFileUpload = new MultiFileUpload(this::onStart, this::handleFile, stateWindow, multiple);
+		multiFileUpload = new MultiFileUpload(this::fileUploadStarted, this::fileUploadFinished, stateWindow, multiple);
 		multiFileUpload.setWidth(null);
 		multiFileUpload.setUploadButtonIcon(ImageIcon.UP_16_ICON.createResource());
 		multiFileUpload.setUploadButtonCaptions(caption == null ? DEFAULT_SINGLE_BUTTON_CAPTION : caption,
