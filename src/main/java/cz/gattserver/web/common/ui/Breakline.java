e43c5fe2c8435c5423a184480ec658eb0234e432
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-09-24 18:47:05 +0200

    Breakline component

---
+++ src/main/java/cz/gattserver/web/common/ui/Breakline.java
@@ -0,0 +1,11 @@
+package cz.gattserver.web.common.ui;
+
+import com.vaadin.flow.component.HtmlComponent;
+import com.vaadin.flow.component.Tag;
+
+@Tag(Tag.BR)
+public class Breakline extends HtmlComponent {
+
+	private static final long serialVersionUID = -7281701879487816431L;
+
+}
