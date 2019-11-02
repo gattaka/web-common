d8cb3ee38211555a31f81b462b07283c37fcd2fa
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-11-02 22:30:45 +0100

    LinkButton

---
+++ src/main/java/cz/gattserver/web/common/ui/LinkButton.java
@@ -0,0 +1,17 @@
+package cz.gattserver.web.common.ui;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+
+public class LinkButton extends Button {
+	private static final long serialVersionUID = 3709000883176555567L;
+
+	public LinkButton(String text, ComponentEventListener<ClickEvent<Button>> clickListener) {
+		super(text, clickListener);
+		addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+		getStyle().set("cursor", "pointer");
+	}
+
+}
